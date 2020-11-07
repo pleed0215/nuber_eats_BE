66c46347038486a3525e43f4fbdbe8e92a15d30a
pleed0215 <pleed0215@icloud.com>
2020-11-07 19:23:55 +0900

    hh

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,10 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantsResolver {
-  @Query(returns => Restaurant)
-  myRestaunrant() {
-    return true;
+  @Query(returns => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
