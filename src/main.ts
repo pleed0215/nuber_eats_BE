b059a17dba73442da55b941490a99e5fc1f072d5
pleed0215 <pleed0215@icloud.com>
2020-11-08 21:53:21 +0900

    mm

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
